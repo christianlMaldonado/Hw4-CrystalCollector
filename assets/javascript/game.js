868ba196314004bcdc88bed7f5923e566a182649
Christian Maldonado <chris.maldonado99@gmail.com>
2019-12-22 20:37:36 -0800

    added 4 clickable buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,18 @@
-// display 4 buttons
+//  display 4 buttons
 
-//the player will be shown a random number at the start of the game. 19 - 120
+//  the player will be shown a random number at the start of the game. 19 - 120
 
-// When the player clicks on a crystal, it will add a specific amount 
-// of points to the player's total score. 1 - 12
+//  When the player clicks on a crystal, it will add a specific amount 
+//  of points to the player's total score. 1 - 12
 
-// The player wins if their total score matches the random number 
-// from the beginning of the game.
+//  The player wins if their total score matches the random number 
+//  from the beginning of the game.
 
-// The player loses if their score goes above the random number.
+//  The player loses if their score goes above the random number.
 
-// The game restarts whenever the player wins or loses.
+//  The game restarts whenever the player wins or loses.
 
-// When the game begins again, the player should see a new random number.
+//  When the game begins again, the player should see a new random number.
 //  Also, all the crystals will have four new hidden values. Of course, 
 //  the user's score (and score counter) will reset to zero.
 
